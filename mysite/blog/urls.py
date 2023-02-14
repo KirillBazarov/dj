7446391c3face2474b043fe1b8da72fe6c271a09
kirill <kirillbazaroff@gmial.com>
2023-02-14 20:53:02 +0300

    подключил кэш

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,8 +1,10 @@
+from django.views.decorators.cache import cache_page
+
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', cache_page(60)(views.PostList.as_view()), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     # path('register/', views.RegisterUser.as_view(), name='register'),
     # path('login/', views.PostDetail.as_view(), name='login'),
