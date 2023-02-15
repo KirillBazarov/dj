604e0675fa7b966c4dce42ca6045fed00c42632b
demka <kirillbazaroff@gmail.com>
2023-02-15 16:05:59 +0300

    добавил регистрацию

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', cache_page(60)(views.PostList.as_view()), name='home'),
+    path('',views.PostList.as_view(), name='home'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    # path('register/', views.RegisterUser.as_view(), name='register'),
     # path('login/', views.PostDetail.as_view(), name='login'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import CreateView
 
+from .forms import RegisterUserForm
 from .models import Post
 
 
@@ -16,12 +17,7 @@ class PostDetail(generic.DetailView):
     template_name = 'post_detail.html'
 
 
-# class RegisterUser(CreateView):
-#     form_class = UserCreationForm
-#     template_name = 'register.html'
-#     success_url = reverse_lazy('login')
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         c_def = self.get_user_context(title="Регистрация")
-#         return dict(list(context.items()) + list(c_def.items()))
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('home')
